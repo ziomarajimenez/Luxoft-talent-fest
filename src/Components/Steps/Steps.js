8dcb023e9dbe58efdc0a8d6247e03d86a88531b6
DenisseMolina <dmolinaruelas@gmail.com>
2022-08-22 18:50:39 -0500

    Botones step

---
+++ src/Components/Steps/Steps.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './steps.css'
+
+const Steps = () => {
+  return (
+    <div className="steps">
+      <div className="step-1">Paso 1</div>
+      <div className="step-2">Paso 2</div>
+      <div className="step-3">Paso 3</div>
+    </div>
+  );
+};
+
+export default Steps;
