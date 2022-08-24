daa464bb4f634a3b545bca2b521cc95cd36bfa04
DenisseMolina <dmolinaruelas@gmail.com>
2022-08-23 20:32:51 -0500

    Vista step 3 terminada

--- src/Pages/Step 3/Step3.js
+++ src/Pages/Step 3/Step3.js
@@ -7,6 +7,9 @@ import frightenmusic from "../../Assets/Frighten music/acoustic-motivation-11290
 import frightenmusic2 from "../../Assets/Frighten music/inspirational-background-112290.mp3";
 import frightenmusic3 from "../../Assets/Frighten music/motivational-day-112790.mp3";
 import frightenmusic4 from "../../Assets/Frighten music/winning-elevation-111355.mp3";
+import info from "../../Assets/Info.png";
+import { emotionsInfo } from "../../Utils/emotionsInfo";
+import { convertLength } from "@mui/material/styles/cssUtils";
 
 const Step3 = () => {
   // const navigate = useNavigate();
@@ -21,7 +24,11 @@ const Step3 = () => {
   let randomMusic =
     musicFrighten[Math.floor(Math.random() * musicFrighten.length)];
 
-  // let phrases = [“”]
+  let { messages } = emotionsInfo;
+
+  let randomPhrases =
+    messages.miedo[Math.floor(Math.random() * messages.miedo.length)];
+  console.log(messages);
 
   return (
     <div className="section-audio">
@@ -29,13 +36,17 @@ const Step3 = () => {
       <section id="audio-instructions">
         <p>Escucha el siguiente audio</p>
       </section>
-      <audio src={randomMusic} controls></audio>
 
-      <p>“Cada día aprendo más sobre cómo canalizar mis emociones”</p>
-      <button onClick={() => setIsOpen(true)}>
-        {" "}
-        Necesito hablar con alguien
-      </button>
+      <audio className="audio" src={randomMusic} controls></audio>
+
+      <p id="random-phrases">"{randomPhrases}"</p>
+      <section className="section-talk">
+        <button className="btn-talk-to-someone" onClick={() => setIsOpen(true)}>
+          {" "}
+          <img id="info" src={info} alt="info" />
+          Necesito hablar con alguien
+        </button>
+      </section>
       <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}></Modal>
       <button>Finalizar</button>
     </div>
