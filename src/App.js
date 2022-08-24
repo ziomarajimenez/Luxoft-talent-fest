a86b91b08c2f5abf3634f52aad5f90d37601600a
DenisseMolina <dmolinaruelas@gmail.com>
2022-08-24 16:37:22 -0500

    App completo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./Pages/Landing Page/LandingPage";
-import { Emotions } from "./Pages/Emotions/Emotions";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { LandingPage } from './Pages/Landing Page/LandingPage'
+import { Emotions } from './Pages/Emotions/Emotions'
 import { Description } from './Pages/Description/Description'
-import Step1 from './Pages/Step 1/Step1';
+// Imports for "not" case
+import { Step1 } from './Pages/Step 1/Step1'
+import { Step2Mandala } from './Pages/Not - Step 2/Step2-Mandala'
 import Step3 from "./Pages/Step 3/Step3";
-
-
+// Imports for "yes" case
+import { Step1Words } from './Pages/Work - Yes - Step1/Step1 - Words';
+import { Step3SelfQuestions } from './Pages/Yes - Step 3/Step 3 - SelfQuestions'
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
         <Routes >
           <Route path='/' element={<LandingPage />} />
           <Route path='/emotions' element={<Emotions />} />
           <Route path='/description' element={<Description />} />
-          <Route path='/step1' element={<Step1 />} /> 
+          <Route path='/step1' element={<Step1 />} />
+          <Route path= '/not-step2' element={<Step2Mandala />} />
           <Route path="/step3" element={<Step3 />} />
+          <Route path="/yes-step1" element={<Step1Words />} />
+          <Route path='/yes-step3' element={<Step3SelfQuestions />} />
         </Routes>
       </BrowserRouter>
       {/* <LandingPage /> */}
-
     </div>
   );
 }
-
 export default App;
