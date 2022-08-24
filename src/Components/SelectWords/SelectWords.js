97d180a5c827c34b145dbf3fef9e90a5cd760c54
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2022-08-24 16:07:24 -0500

    Se completa funcionalidad de SelectWords y vista correspondiente. Condicionado de botón Continuar. Ajustes en estilos globales para MainButton.

--- src/Components/SelectWords/SelectWords.js
+++ src/Components/SelectWords/SelectWords.js
@@ -1,47 +1,70 @@
 import React, {useState, useEffect} from "react"
 import { emotionsInfo } from "../../Utils/emotionsInfo"
 import '../SelectWords/SelectWords.css' 
-function SelectWords() {
 
+const  SelectWords =(props)=>  {
+    const {setDisabledButton} = props
     let { words } = emotionsInfo
     const [options, setOptions] = useState(words.miedo)
     const [selections, setSelections] = useState([])
 
-    
+    useEffect(() => {
+        if(selections.length > 3 || selections.length < 3 ) {
+            setDisabledButton(true)
+        }else {
+            setDisabledButton(false)
+        }
+    }, [selections]);
+
     useEffect(() => {
         console.log("options", options)
         console.log("selections", selections)
 
     }, []);
 
-    const handleClick = (option) => {
+    const handleClickOption = (option) => {
         console.log("option selected => ", option)
-        setSelections(current => [...current, option])
-
+        if (selections.length < 3) {
+            setSelections(current => [...current, option])
+            setOptions(current => current.filter(element => {
+                return element !== option
+        }))}
 
     }
+
+    const handleClickSelection = (selection) => {
+        console.log("selection clicked => ", selection)
+        setOptions(current => [...current, selection])
+        setSelections(current => current.filter(element => {
+            return element !== selection
+        }))
+    }
+
+
 
     return (
         <>
         <div className="columnContainer">
-            <div className="columnInfo optionsColumn">
-                {options.map( (option) =>  (
-                    <div id={option} className="option" onClick={()=> handleClick(option)}> 
-                    <p>{option}</p>
-                     </div>
-                ) )}
-                
+            <div className="columnSpace">
+                <div className="columnInfo optionsColumn">
+                    {options.map( (option) =>  (
+                        <div id={option} className="option" onClick={()=> handleClickOption(option)}> 
+                            <p className="word">{option}</p>
+                        </div>
+                    ) )}
+                    
+                </div>
             </div>
-            <div className="columnInfo selectionColumn">
-            { selections.length ? selections.map( (selection) =>  (
-                    <div id={selection} className="selection" 
-                    // onClick={()=> handleClick(selection)}
-                    > 
-                    <p>{selection}</p>
-                     </div>
-                ) )
-             : ''
-            }
+            <div className="columnSpace">
+                <div className="columnInfo selectionColumn">
+                { selections.length ? selections.map( (selection) =>  (
+                        <div id={selection} className="selection" onClick={()=> handleClickSelection(selection)}> 
+                            <p className="word">{selection}</p>
+                        </div>
+                    ) )
+                : ''
+                }
+                </div>
             </div>
         </div>
         </>
